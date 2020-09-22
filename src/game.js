f2312f357dbd9970d0cf790f58e6c711ac140808
Adam Sornoso <asornoso@yahoo.com>
2020-09-22 12:28:22 -0700

    made global game.js variables part of scene variables as scene_variables object

--- src/game.js
+++ src/game.js
@@ -12,22 +12,22 @@ let game_scene = new Phaser.Scene('game_scene')
 let width = window.innerWidth;
 let height = window.innerHeight
 
-let speed = 15
-let score = 0
-let lives = 4
-let topping_sprite_list = [
-  "toppings_cheese", "toppings_chicken", "toppings_ham",
-  "toppings_mushroom", "toppings_olive", "toppings_onion",
-  "toppings_bell_pepper_1", "toppings_bell_pepper_2",
-   "toppings_pineapple", "toppings_tomato"
-]
-let spawn_time = 1800 //ms
-let last_spawn = Date.now()
-let move_left = false
-let move_right = false
-
 
 game_scene.create = function() {
+  this.scene_variables = {}
+  this.scene_variables.speed = 15
+  this.scene_variables.score = 0
+  this.scene_variables.lives = 4
+  this.scene_variables.topping_sprite_list = [
+    "toppings_cheese", "toppings_chicken", "toppings_ham",
+    "toppings_mushroom", "toppings_olive", "toppings_onion",
+    "toppings_bell_pepper_1", "toppings_bell_pepper_2",
+     "toppings_pineapple", "toppings_tomato"
+  ]
+  this.scene_variables.spawn_time = 1800 //ms
+  this.scene_variables.last_spawn = Date.now()
+  this.scene_variables.move_left = false
+  this.scene_variables.move_right = false
 
   this.toppings_group = this.physics.add.group()
   this.bad_toppings_group = this.physics.add.group()
@@ -56,44 +56,43 @@ game_scene.create = function() {
 
   //Move player on touch down
   this.left_control.on('pointerdown', ()=> {
-    move_left = true
-    move_right = false
+    this.scene_variables.move_left = true
+    this.scene_variables.move_right = false
   })
   this.right_control.on('pointerdown', ()=> {
-    move_right = true
-    move_left = false
+    this.scene_variables.move_right = true
+    this.scene_variables.move_left = false
   })
 
   //Move player on touch down
   this.left_control.on('pointerup', ()=> {
-    move_left = false
+    this.scene_variables.move_left = false
   })
   this.right_control.on('pointerup', ()=> {
-    move_right = false
+    this.scene_variables.move_right = false
   })
 
   //Setup game text
   //Score:
   this.score_text = this.make.text({
-    x: width * 0.02, y: 10, text: "Score: "+score,
+    x: width * 0.02, y: 10, text: "Score: "+this.scene_variables.score,
     style: {...constants.font_configs.m, color: "#ffffff"}
   })
 
   //Lives:
   this.lives_text = this.make.text({
-    x: width * 0.75, y: 10, text: "Lives: "+lives,
+    x: width * 0.75, y: 10, text: "Lives: "+this.scene_variables.lives,
     style: {...constants.font_configs.m, color: "#ffffff"}
   })
-
 
 }
 
 game_scene.update = function(){
 
-  if(move_right)
-    this.player_sprite.x += speed
-  else if(move_left)
-    this.player_sprite.x -= speed
+  if(this.scene_variables.move_right)
+    this.player_sprite.x += this.scene_variables.speed
+  else if(this.scene_variables.move_left)
+    this.player_sprite.x -= this.scene_variables.speed
 
 
   //Generate toppings
@@ -101,31 +100,32 @@ game_scene.update = function(){
   //70% of the time generate good toppping
   //Generate 10 toppings per min at random intervals
   let now = Date.now()
-  if(now - last_spawn >= spawn_time)
+  if(now - this.scene_variables.last_spawn >= this.scene_variables.spawn_time)
   {
     let t
     if(Math.random() < 0.3){
-      t = this.bad_toppings_group.create(Math.floor(Math.random() * width - 200) + 100, -200, topping_sprite_list[Math.floor(Math.random() * 10)])
+      t = this.bad_toppings_group.create(Math.floor(Math.random() * width - 200) + 100, -200, this.scene_variables.topping_sprite_list[Math.floor(Math.random() * 10)])
       t.tint = 0x800080
     }
     else
-      t = this.toppings_group.create(Math.floor(Math.random() * width - 200) + 100, -200, topping_sprite_list[Math.floor(Math.random() * 10)])
+      t = this.toppings_group.create(Math.floor(Math.random() * width - 200) + 100, -200, this.scene_variables.topping_sprite_list[Math.floor(Math.random() * 10)])
     t.setScale(3)
-    last_spawn = now
+    this.scene_variables.last_spawn = now
+    this.scene_variables.spawn_time -= 10
   }
 
 
   //Detect collisions between player and good toppings to increase score
   this.physics.add.overlap(this.player_sprite, this.toppings_group, (player, topping) => {
-    score += 1
-    this.score_text.setText("Score: "+ score)
+    this.scene_variables.score += 1
+    this.score_text.setText("Score: "+ this.scene_variables.score)
     topping.destroy()
   }, null, this);
 
   //Detect collisions between player and bad toppings to reduce lives
   this.physics.add.overlap(this.player_sprite, this.bad_toppings_group, (player, topping) => {
-    lives -= 1
-    this.lives_text.setText("Lives: "+ lives)
+    this.scene_variables.lives -= 1
+    this.lives_text.setText("Lives: "+ this.scene_variables.lives)
     topping.destroy()
   }, null, this);
 
@@ -138,15 +138,11 @@ game_scene.update = function(){
   }
 
   //Check for end game
-  if(lives <= 0){
+  if(this.scene_variables.lives <= 0){
     this.scene.stop('game_scene')
     this.scene.start('menu_scene')
   }
-
-
-
 }
-
 
 
 export default game_scene
